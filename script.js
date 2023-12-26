0dd1396d1523e3ba44d3323156b8bf3cd63cfec9
Ines Repnik <ines.repnik@gmail.com>
2023-12-26 12:34:15 +0100

    Changing the gameplay a bit

--- script.js
+++ script.js
@@ -6,39 +6,56 @@ function getComputerChoice() {
     return(shapes[random]);
 }
 
-function playRound(playerSelection, computerSelection){
-    if(playerSelection === computerSelection){
-        return(console.log("It is a tie. Please play again!"));
-
-    } else{
-        if((computerSelection === "rock") & (playerSelection === "scissors")) {
-            return(console.log("Rock crushes scissors! You lose!"));
-        }
-        else if((computerSelection === "rock") & (playerSelection === "paper")){
-            return(console.log("Paper covers rock. You win!"));
-        }
-        else if((computerSelection === "paper") & (playerSelection === "rock")){
-            return(console.log("Paper beats rock. You lose!"));
-        }
-        else if((computerSelection === "paper") & (playerSelection === "scissors")){
-            return(console.log("Scissors beat paper. You win!"));
-        }
-        else if((computerSelection === "scissors") & (playerSelection === "rock")){
-            return(console.log("Rock beats scissors. You win!"));
-
-        }
-        else if((computerSelection === "scissors") && (playerSelection === "paper")){
-            return(console.log("Scissors beat paper. You lose!"));
-        }
-    }
-
+function getPlayerSelection() {
+    const playerSelection = (prompt('Please insert rock, paper or scissors: ')).toLowerCase();
+    console.log(playerSelection);
+    return(playerSelection);
 }
 
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
+function playRound(playerSelection, computerSelection){
+        let tie = 0;
+        let playerWin = 0;
+        let computerWin = 0;
+        if(playerSelection === computerSelection){
+            tie++;
+            return(console.log("It is a tie. Please play again!"));
+    
+        } else{
+            if((computerSelection === "rock") & (playerSelection === "scissors")) {
+                computerWin++;
+                console.log(computerWin);
+                return(console.log("Rock crushes scissors! You lose!"));
+            }
+            else if((computerSelection === "rock") & (playerSelection === "paper")){
+                playerWin++;
+                return(console.log("Paper covers rock. You win!"));
+            }
+            else if((computerSelection === "paper") & (playerSelection === "rock")){
+                computerWin++;
+                return(console.log("Paper beats rock. You lose!"));
+            }
+            else if((computerSelection === "paper") & (playerSelection === "scissors")){
+                playerWin++;
+                return(console.log("Scissors beat paper. You win!"));
+            }
+            else if((computerSelection === "scissors") & (playerSelection === "rock")){
+                playerWin++;
+                return(console.log("Rock beats scissors. You win!"));
+    
+            }
+            else if((computerSelection === "scissors") && (playerSelection === "paper")){
+                computerWin++;
+                return(console.log("Scissors beat paper. You lose!"));
+            }
+        }
 
-const playerSelection = (prompt('Please insert rock, paper or scissors: ')).toLowerCase();
-console.log(playerSelection);
+    }
+
+
+function game(){
+    playRound(playerSelection, computerSelection);
+}
+
 
 //playRound(playerSelection, computerSelection);
 //console.log(playRound(playerSelection, computerSelection));
